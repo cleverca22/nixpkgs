c865a2a7dd68d0d7f3f7b5bdda951feba0bc05c4
Michael Bishop <cleverca22@gmail.com>
2021-11-25 16:06:24 -0400

    python3Packages.dbus-python: fix cross-compilation
    (cherry picked from commit a3adc9674969caf49662bb4b2be725d285833377)

--- pkgs/development/python-modules/dbus/default.nix
+++ pkgs/development/python-modules/dbus/default.nix
@@ -1,40 +1,63 @@
-{ lib, stdenv, fetchPypi, buildPythonPackage, python, pkg-config, dbus, dbus-glib, isPyPy
-, ncurses, pygobject3, isPy3k }:
+{ stdenv
+, lib
+, fetchFromGitLab
+, autoreconfHook
+, autoconf-archive
+, pkg-config
+, python3
+, pygobject3
+, dbus
+, dbus-glib
+, ncurses
+}:
 
-buildPythonPackage rec {
+with lib;
+
+stdenv.mkDerivation rec {
   pname = "dbus-python";
   version = "1.2.18";
-  format = "other";
 
   outputs = [ "out" "dev" ];
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "0q3jrw515z98mqdk9x822nd95rky455zz9876f1nqna5igkd3gcj";
+  src = fetchFromGitLab {
+    domain = "gitlab.freedesktop.org";
+    owner = "dbus";
+    repo = pname;
+    rev = "${pname}-${version}";
+    sha256 = "147nwvl3b92vramsmxnfqjmalp4yj13v0lyxzajfkszgyl3xwbxs";
   };
 
   patches = [
     ./fix-includedir.patch
+    ./fix-cross-compilation.patch
   ];
 
-  disabled = isPyPy;
+  autoreconfPhase = "NOCONFIGURE=1 ./autogen.sh";
 
-  preConfigure = if (lib.versionAtLeast stdenv.hostPlatform.darwinMinVersion "11" && stdenv.isDarwin) then ''
-    MACOSX_DEPLOYMENT_TARGET=10.16
-  '' else null;
+  preConfigure =
+    if (lib.versionAtLeast stdenv.hostPlatform.darwinMinVersion "11" && stdenv.isDarwin) then ''
+      MACOSX_DEPLOYMENT_TARGET=10.16
+    '' else null;
 
-  nativeBuildInputs = [ pkg-config ];
+  nativeBuildInputs = [
+    pkg-config
+    autoconf-archive
+    autoreconfHook
+    python3
+  ];
+
   buildInputs = [ dbus dbus-glib ]
     # My guess why it's sometimes trying to -lncurses.
     # It seems not to retain the dependency anyway.
-    ++ lib.optional (! python ? modules) ncurses;
+    ++ lib.optional (! python3 ? modules) ncurses;
 
-  doCheck = isPy3k;
   checkInputs = [ dbus.out pygobject3 ];
 
   meta = {
     description = "Python DBus bindings";
+    homepage = "https://gitlab.freedesktop.org/dbus/dbus-python";
     license = lib.licenses.mit;
     platforms = dbus.meta.platforms;
   };
+
 }
